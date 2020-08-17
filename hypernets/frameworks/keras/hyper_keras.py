c3a462b6928432762115e175fe5a7b393a8c6059
yangjian <yangjian@zetyun.com>
2020-08-17 19:33:11 +0800

    Support training One-shot model in HyperKeras

--- hypernets/frameworks/keras/hyper_keras.py
+++ hypernets/frameworks/keras/hyper_keras.py
@@ -8,10 +8,13 @@ from hypernets.model.estimator import Estimator
 
 from tensorflow.keras import backend as K
 from tensorflow.keras import utils
-
+import tensorflow as tf
 import numpy as np
 import gc
 from .layer_weights_cache import LayerWeightsCache
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class KerasEstimator(Estimator):
@@ -55,7 +58,8 @@ class KerasEstimator(Estimator):
 
 class HyperKeras(HyperModel):
     def __init__(self, searcher, optimizer, loss, metrics, dispatcher=None, callbacks=[],
-                 reward_metric=None, max_model_size=0, one_shot_mode=False, visualization=False):
+                 reward_metric=None, max_model_size=0, one_shot_mode=False, one_shot_training_sampler=None,
+                 visualization=False):
         self.optimizer = optimizer
         self.loss = loss
         self.metrics = metrics
@@ -66,6 +70,8 @@ class HyperKeras(HyperModel):
             self.weights_cache = LayerWeightsCache()
         else:
             self.weights_cache = None
+        self.one_shot_mode = one_shot_mode
+        self.one_shot_training_sampler = one_shot_training_sampler if one_shot_training_sampler is not None else searcher
         self.visualization = visualization
         HyperModel.__init__(self, searcher, dispatcher=dispatcher, callbacks=callbacks, reward_metric=reward_metric)
 
@@ -74,6 +80,30 @@ class HyperKeras(HyperModel):
                                    max_model_size=self.max_model_size, weights_cache=self.weights_cache,
                                    visualization=self.visualization)
         return estimator
+
+    def build_dataset_iter(self, X, y, batch_size=32, buffer_size=None, reshuffle_each_iteration=None, repeat_count=1):
+        if buffer_size is None:
+            buffer_size = len(X[-1])
+        dataset = tf.data.Dataset. \
+            from_tensor_slices((X, y)). \
+            shuffle(buffer_size=buffer_size, reshuffle_each_iteration=reshuffle_each_iteration). \
+            repeat(repeat_count). \
+            batch(batch_size)
+
+        return iter(dataset)
+
+    def fit_one_shot_model_epoch(self, X, y, batch_size=32, steps=None, epoch_no=0):
+        step = 0
+        dataset_iter = self.build_dataset_iter(X, y, batch_size=batch_size)
+        for X_batch, y_batch in dataset_iter:
+            sample = self.one_shot_training_sampler.sample()
+            est = self._get_estimator(space_sample=sample)
+            est.fit(X_batch, y_batch, batch_size=batch_size, epochs=1)
+            step += 1
+            print(f'One-shot model training, Epoch[{epoch_no}], Step[{step}]')
+            if steps is not None and step >= steps:
+                break
+        print(f'One-shot model training finished. Epoch[{epoch_no}], Step[{step}]')
 
     def export_trail_configuration(self, trail):
         return None
--- tests/keras/hyper_keras_test.py
+++ tests/keras/hyper_keras_test.py
@@ -11,30 +11,48 @@ from hypernets.core.searcher import OptimizeDirection
 
 
 class Test_HyperKeras():
-    def test_model_with_hp(self):
-        def get_space():
-            space = HyperSpace()
-            with space.as_default():
-                in1 = Input(shape=(10,))
-                in2 = Input(shape=(20,))
-                in3 = Input(shape=(1,))
-                concat = Concatenate()([in1, in2, in3])
-                dense1 = Dense(10, activation=Choice(['relu', 'tanh', None]), use_bias=Bool())(concat)
-                bn1 = BatchNormalization()(dense1)
-                dropout1 = Dropout(Choice([0.3, 0.4, 0.5]))(bn1)
-                output = Dense(2, activation='softmax', use_bias=True)(dropout1)
-            return space
+    def get_space(self):
+        space = HyperSpace()
+        with space.as_default():
+            in1 = Input(shape=(10,))
+            in2 = Input(shape=(20,))
+            in3 = Input(shape=(1,))
+            concat = Concatenate()([in1, in2, in3])
+            dense1 = Dense(10, activation=Choice(['relu', 'tanh', None]), use_bias=Bool())(concat)
+            bn1 = BatchNormalization()(dense1)
+            dropout1 = Dropout(Choice([0.3, 0.4, 0.5]))(bn1)
+            output = Dense(2, activation='softmax', use_bias=True)(dropout1)
+        return space
 
-        rs = RandomSearcher(get_space, optimize_direction=OptimizeDirection.Maximize)
-        hk = HyperKeras(rs, optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'],
-                        callbacks=[SummaryCallback()])
+    def get_space_simple(self):
+        space = HyperSpace()
+        with space.as_default():
+            in1 = Input(shape=(10,))
+            dense1 = Dense(10, activation=Choice(['relu', 'tanh', None]), use_bias=Bool())(in1)
+            bn1 = BatchNormalization()(dense1)
+            dropout1 = Dropout(Choice([0.3, 0.4, 0.5]))(bn1)
+            output = Dense(2, activation='softmax', use_bias=True)(dropout1)
+        return space
 
+    def get_x_y(self):
         x1 = np.random.randint(0, 10000, size=(100, 10))
         x2 = np.random.randint(0, 100, size=(100, 20))
         x3 = np.random.normal(1.0, 100.0, size=(100))
         y = np.random.randint(0, 2, size=(100), dtype='int')
         x = [x1, x2, x3]
+        return x, y
 
+    def get_x_y_1(self):
+        x = np.random.randint(0, 10000, size=(100, 10))
+        y = np.random.randint(0, 2, size=(100), dtype='int')
+        return x, y
+
+    def test_model_with_hp(self):
+        rs = RandomSearcher(self.get_space, optimize_direction=OptimizeDirection.Maximize)
+        hk = HyperKeras(rs, optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'],
+                        callbacks=[SummaryCallback()])
+
+        x, y = self.get_x_y()
         hk.search(x, y, x, y, max_trails=3)
         assert hk.best_model
         best_trial = hk.get_best_trail()
@@ -44,3 +62,59 @@ class Test_HyperKeras():
         result = estimator.evaluate(x, y)
         assert len(score) == 100
         assert result
+
+    def test_build_dataset_iter(self):
+        rs = RandomSearcher(self.get_space, optimize_direction=OptimizeDirection.Maximize)
+        hk = HyperKeras(rs, optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'],
+                        callbacks=[SummaryCallback()])
+        x, y = self.get_x_y_1()
+
+        ds_iter = hk.build_dataset_iter(x, y, batch_size=10)
+
+        batch_counter = 0
+        for x_b, y_b in ds_iter:
+            # x_b, y_b = next()
+            assert len(x_b) == 10
+            assert len(y_b) == 10
+            batch_counter += 1
+        assert batch_counter == 10
+
+        ds_iter = hk.build_dataset_iter(x, y, batch_size=32)
+
+        batch_counter = 0
+        for x_b, y_b in ds_iter:
+            # x_b, y_b = next()
+            if batch_counter < 3:
+                assert len(x_b) == 32
+                assert len(y_b) == 32
+            else:
+                assert len(x_b) == 4
+                assert len(y_b) == 4
+            batch_counter += 1
+        assert batch_counter == 4
+
+        ds_iter = hk.build_dataset_iter(x, y, batch_size=32, repeat_count=2)
+
+        batch_counter = 0
+        for x_b, y_b in ds_iter:
+            # x_b, y_b = next()
+            if batch_counter < 6:
+                assert len(x_b) == 32
+                assert len(y_b) == 32
+            else:
+                assert len(x_b) == 8
+                assert len(y_b) == 8
+            batch_counter += 1
+        assert batch_counter == 7
+
+    def test_fit_one_shot_model_epoch(self):
+        rs = RandomSearcher(self.get_space_simple, optimize_direction=OptimizeDirection.Maximize)
+        hk = HyperKeras(rs,
+                        optimizer='adam',
+                        loss='sparse_categorical_crossentropy',
+                        metrics=['accuracy'],
+                        callbacks=[SummaryCallback()],
+                        one_shot_mode=True,
+                        one_shot_training_sampler=rs)
+        x, y = self.get_x_y_1()
+        hk.fit_one_shot_model_epoch(x, y)
