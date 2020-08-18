a3b8dbbbb44ebfc5d835606e526da4486c7a0831
yangjian <yangjian@zetyun.com>
2020-08-18 16:48:23 +0800

    Add OneShotModel

--- hypernets/frameworks/keras/one_shot_model.py
+++ hypernets/frameworks/keras/one_shot_model.py
@@ -9,24 +9,29 @@ from ...core.trial import Trail
 import time
 
 
-class EnasModel(HyperKeras):
+class OneShotModel(HyperKeras):
     def __init__(self, searcher, optimizer, loss, metrics, epochs=320, batch_size=64, controller_train_steps=50,
                  dispatcher=None, callbacks=[], reward_metric=None, max_model_size=0, visualization=False):
         self.epochs = epochs
         self.batch_size = batch_size
         self.controller_train_steps = controller_train_steps
-        super(EnasModel, self).__init__(searcher, optimizer, loss, metrics,
-                                        dispatcher=dispatcher,
-                                        callbacks=callbacks,
-                                        reward_metric=reward_metric,
-                                        max_model_size=max_model_size,
-                                        one_shot_mode=True,
-                                        one_shot_training_sampler=searcher,
-                                        visualization=visualization)
+        super(OneShotModel, self).__init__(searcher, optimizer, loss, metrics,
+                                           dispatcher=dispatcher,
+                                           callbacks=callbacks,
+                                           reward_metric=reward_metric,
+                                           max_model_size=max_model_size,
+                                           one_shot_mode=True,
+                                           one_shot_training_sampler=searcher,
+                                           visualization=visualization)
 
     def search(self, X, y, X_val, y_val, max_trails=10, dataset_id=None, trail_store=None, **fit_kwargs):
+        self.start_search_time = time.time()
+
         trail_no = 1
         for epoch in range(self.epochs):
+            if trail_no >= max_trails:
+                break
+
             print(f'One-shot model epoch({epoch}) training...')
             self.fit_one_shot_model_epoch(X, y, batch_size=self.batch_size, epoch=epoch)
 
@@ -34,7 +39,7 @@ class EnasModel(HyperKeras):
             for controller_step in range(self.controller_train_steps):
                 start_time = time.time()
                 space_sample = self.searcher.sample()
-                estimator = self._get_estimator()
+                estimator = self._get_estimator(space_sample)
                 for callback in self.callbacks:
                     callback.on_build_estimator(self, space_sample, estimator, trail_no)
                     callback.on_trail_begin(self, space_sample, trail_no)
