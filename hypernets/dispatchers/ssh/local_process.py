871d82b29790534b16260982127dcc9f8a39d899
lixfz <lixf@sovon.net>
2020-09-09 19:33:34 +0800

    adjust process output buffering policy

--- hypernets/dispatchers/ssh/local_process.py
+++ hypernets/dispatchers/ssh/local_process.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 
-from multiprocessing import Process, Value as PValue
 import subprocess
 import sys
+from multiprocessing import Process, Value as PValue
 
 
 class LocalProcess(Process):
@@ -16,32 +16,28 @@ class LocalProcess(Process):
         self._exit_code = PValue('i', -1)
 
     def run(self):
+        print(f'[CMD] {self.cmd}, out={self.out_file}, err={self.err_file}')
+
         if self.out_file and self.err_file:
-            with open(self.out_file, 'wb')as o, open(self.err_file, 'wb') as e:
+            with open(self.out_file, 'wb', buffering=0)as o, open(self.err_file, 'wb', buffering=0) as e:
                 p = subprocess.run(self.cmd.split(' '),
-                                   shell=True,
-                                   stdin=None,
+                                   shell=False,
+                                   stdin=subprocess.DEVNULL,
                                    stdout=o,
                                    stderr=e)
                 code = p.returncode
         else:
             p = subprocess.run(self.cmd.split(' '),
-                               shell=True,
-                               stdin=None,
+                               shell=False,
+                               stdin=subprocess.DEVNULL,
                                stdout=sys.stdout,
                                stderr=sys.stderr)
             code = p.returncode
 
+        print(f'[CMD] done with {code}')
         self._exit_code.value = code
 
     @property
     def exitcode(self):
         code = self._exit_code.value
         return code if code >= 0 else None
-
-
-if __name__ == '__main__':
-    p = LocalProcess('ls', None, 'my.out', 'my.err')
-    p.start()
-    p.join()
-    print(p.cmd, 'exit with', p.exitcode)
