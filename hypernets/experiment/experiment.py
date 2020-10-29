aca620e3f00b1521f8d0c7043569db8dd12b9039
yangjian <yangjian@zetyun.com>
2020-10-29 14:01:29 +0800

    Combine feature generation and selection to one transformer

--- hypernets/experiment/experiment.py
+++ hypernets/experiment/experiment.py
@@ -8,20 +8,32 @@ from sklearn.model_selection import train_test_split
 
 
 class Experiment():
-    def __init__(self, strategy=None):
+    def __init__(self, hyper_model, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3, strategy=None):
         self.id = None
         self.title = None
         self.description = None
         self.dataset_id = None
         self.path = None
+
+        self.hyper_model = hyper_model
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.X_eval = X_eval
+        self.y_eval = y_eval
+        self.eval_size = eval_size
+
         self.start_time = None
         self.end_time = None
 
         self.stragety = strategy if strategy is not None else ExperimentStragety()
 
-    def run(self, hyper_model, X_train, y_train, X_test, eval_size=0.3, X_eval=None, y_eval=None, **kwargs):
+    def run(self, **kwargs):
         self.start_time = time.time()
-        model = self.stragety.run(hyper_model, X_train, y_train, X_test, **kwargs)
+        X_train, y_train, X_test, X_eval, y_eval = self.stragety.data_split(self.X_train, self.y_train,
+                                                                            self.X_test, self.X_eval,
+                                                                            self.y_eval, self.eval_size)
+        model = self.stragety.train(self.hyper_model, X_train, y_train, X_test, X_eval=X_eval, y_eval=y_eval, **kwargs)
         self.end_time = time.time()
         return model
 
@@ -34,7 +46,10 @@ class Experiment():
 
 
 class ExperimentStragety():
-    def run(self, hyper_model, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3, **kwargs):
+    def data_split(self, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3):
+        raise NotImplementedError
+
+    def train(self, hyper_model, X_train, y_train, X_test, X_eval=None, y_eval=None, **kwargs):
         raise NotImplementedError
 
 
@@ -42,7 +57,13 @@ class DefaultExperimentStragety(ExperimentStragety):
     def __init__(self, random_state=9527):
         self.random_state = random_state
 
-    def run(self, hyper_model, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3, **kwargs):
+    def data_split(self, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3):
+        if X_eval or y_eval is None:
+            X_train, X_eval, y_train, y_eval = train_test_split(X_train, y_train, test_size=eval_size,
+                                                                random_state=self.random_state, stratify=y_train)
+        return X_train, y_train, X_test, X_eval, y_eval
+
+    def train(self, hyper_model, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3, **kwargs):
         """Run an experiment
 
         Arguments
