80c0d1dea66632713746f4f3b55b9dd5655e2421
yangjian <yangjian@zetyun.com>
2020-12-01 13:53:15 +0800

    Add callbacks in experiments

--- hypernets/experiment/_experiment.py
+++ hypernets/experiment/_experiment.py
@@ -5,38 +5,45 @@ __author__ = 'yangjian'
 """
 import time
 
+from hypernets.utils import logging
+
+logger = logging.get_logger(__name__)
+
 
 class ExperimentCallback():
-    def experiment_start(self):
+    def experiment_start(self, exp):
         pass
 
-    def experiment_end(self):
+    def experiment_end(self, exp, elapsed):
         pass
 
-    def experiment_break(self, error):
+    def experiment_break(self, exp, error):
         pass
 
-    def step_start(self, step):
+    def step_start(self, exp, step):
         pass
 
-    def step_progress(self, step, progress):
+    def step_progress(self, exp, step, progress, elapsed, eta=None):
         pass
 
-    def step_end(self, step, output):
+    def step_end(self, exp, step, output, elapsed):
         pass
 
-    def step_break(self, step, error):
+    def step_break(self, exp, step, error):
         pass
 
 
 class Experiment():
     def __init__(self, hyper_model, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3,
+                 callbacks=None,
                  random_state=9527):
         self.id = None
         self.title = None
         self.description = None
         self.dataset_id = None
         self.path = None
+        self.current_step = None
+        self.step_start_time = None
 
         self.hyper_model = hyper_model
         self.X_train = X_train
@@ -45,6 +52,7 @@ class Experiment():
         self.X_eval = X_eval
         self.y_eval = y_eval
         self.eval_size = eval_size
+        self.callbacks = callbacks if callbacks is not None else []
         self.random_state = random_state
 
         self.start_time = None
@@ -52,14 +60,47 @@ class Experiment():
 
     def run(self, **kwargs):
         self.start_time = time.time()
+        try:
+            for callback in self.callbacks:
+                callback.experiment_start(self)
 
-        model = self.train(self.hyper_model, self.X_train, self.y_train, self.X_test, X_eval=self.X_eval,
-                           y_eval=self.y_eval, **kwargs)
-        self.end_time = time.time()
-        return model
+            model = self.train(self.hyper_model, self.X_train, self.y_train, self.X_test, X_eval=self.X_eval,
+                               y_eval=self.y_eval, **kwargs)
 
-    def data_split(self, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3):
-        raise NotImplementedError
+            self.end_time = time.time()
+
+            for callback in self.callbacks:
+                callback.experiment_end(self, self.elapsed)
+            return model
+        except Exception as e:
+            import traceback
+            msg = f'ExperiementID:[{self.id}] - {self.current_step}: {e}\n{traceback.format_exc()}'
+            logger.error(msg)
+            for callback in self.callbacks:
+                callback.experiment_break(self, msg)
+
+    def step_start(self, step):
+        if self.current_step is not None:
+            raise RuntimeError('Make sure `step_end()` is called at the end of the previous step.')
+
+        self.current_step = step
+        self.step_start_time = time.time()
+        for callback in self.callbacks:
+            callback.step_start(self, step)
+
+    def step_end(self, output=None):
+        if self.current_step is None:
+            raise RuntimeError('Make sure `step_start()` is called at the start of the current step.')
+        elapsed = time.time() - self.step_start_time
+        for callback in self.callbacks:
+            callback.step_end(self, self.current_step, output, elapsed)
+        self.current_step = None
+        self.step_start_time = None
+
+    def step_progress(self, progress, eta=None):
+        elapsed = time.time() - self.step_start_time
+        for callback in self.callbacks:
+            callback.step_progress(self, self.current_step, progress, elapsed, eta)
 
     def train(self, hyper_model, X_train, y_train, X_test, X_eval=None, y_eval=None, **kwargs):
         """Run an experiment
--- hypernets/experiment/general.py
+++ hypernets/experiment/general.py
@@ -6,14 +6,18 @@ __author__ = 'yangjian'
 
 from sklearn.model_selection import train_test_split
 
+from hypernets.utils import logging
 from . import Experiment
+
+logger = logging.get_logger(__name__)
 
 
 class GeneralExperiment(Experiment):
     def __init__(self, hyper_model, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3,
-                 random_state=9527):
-        super().__init__(hyper_model, X_train, y_train, X_test, X_eval=X_eval, y_eval=y_eval, eval_size=eval_size,
-                         random_state=random_state)
+                 callbacks=None, random_state=9527):
+        super(GeneralExperiment, self).__init__(hyper_model, X_train, y_train, X_test, X_eval=X_eval, y_eval=y_eval,
+                                                eval_size=eval_size,
+                                                callbacks=callbacks, random_state=random_state)
 
     def train(self, hyper_model, X_train, y_train, X_test, X_eval=None, y_eval=None, eval_size=0.3, **kwargs):
         """Run an experiment
@@ -23,12 +27,19 @@ class GeneralExperiment(Experiment):
         max_trails :
 
         """
+        self.step_start('data split')
         if X_eval or y_eval is None:
             X_train, X_eval, y_train, y_eval = train_test_split(X_train, y_train, test_size=eval_size,
                                                                 random_state=self.random_state, stratify=y_train)
+        self.step_end()
 
+        self.step_start('search')
         hyper_model.search(X_train, y_train, X_eval, y_eval, **kwargs)
         best_trial = hyper_model.get_best_trail()
+        self.step_end(output={'best_trail': best_trial})
 
-        self.estimator = hyper_model.final_train(best_trial.space_sample, X_train, y_train)
+        self.step_start('load estimator')
+        self.estimator = hyper_model.load_estimator(best_trial.model_file)
+        self.step_end(output={'estimator': self.estimator})
+
         return self.estimator
