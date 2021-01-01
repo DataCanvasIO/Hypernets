13c0ec165ccdf01675aa55325122d35efa86c4a6
yangjian <yangjian@zetyun.com>
2021-01-01 08:21:39 +0800

    Add isnotebook

--- hypernets/utils/common.py
+++ hypernets/utils/common.py
@@ -68,3 +68,16 @@ class Counter(object):
         with self._lock:
             self._value = 0
             return self._value
+
+
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except:
+        return False
