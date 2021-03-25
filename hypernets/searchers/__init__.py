885ff0466a95e5479b0474a9c3e25345d7b70dde
yangjian <yangjian@zetyun.com>
2021-03-25 18:33:36 +0800

    Add PlaybackSearcher

--- hypernets/searchers/__init__.py
+++ hypernets/searchers/__init__.py
@@ -7,3 +7,4 @@ __author__ = 'yangjian'
 from .evolution_searcher import EvolutionSearcher
 from .mcts_searcher import MCTSSearcher
 from .random_searcher import RandomSearcher
+from .playback_searcher import PlaybackSearcher
---
+++ hypernets/searchers/playback_searcher.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+"""
+
+"""
+from ..core.searcher import Searcher, OptimizeDirection
+from ..core import TrialHistory
+from ..core.callbacks import EarlyStoppingError
+
+
+class PlaybackSearcher(Searcher):
+    def __init__(self, trail_history: TrialHistory, top_n=20):
+        assert trail_history is not None
+        assert len(trail_history.history) > 0
+        self.history = trail_history
+        self.top_n = top_n
+        self.samples = [t.space_sample for t in self.history.get_top(top_n)]
+        self.index = 0
+        Searcher.__init__(self, None, use_meta_learner=False)
+
+    @property
+    def parallelizable(self):
+        return True
+
+    def sample(self):
+        if self.index >= len(self.samples):
+            raise EarlyStoppingError('no more samples.')
+        sample = self.samples[self.index]
+        self.index += 1
+        return sample
+
+    def update_result(self, space, result):
+        pass
