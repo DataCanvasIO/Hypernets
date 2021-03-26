fed00aaf3f59efb63a2615c6020934b6df33ff91
yangjian <yangjian@zetyun.com>
2021-03-26 15:37:43 +0800

    Add args to __init__ of PlaybackSearcher

--- hypernets/searchers/playback_searcher.py
+++ hypernets/searchers/playback_searcher.py
@@ -8,14 +8,14 @@ from ..core.callbacks import EarlyStoppingError
 
 
 class PlaybackSearcher(Searcher):
-    def __init__(self, trail_history: TrialHistory, top_n=20):
+    def __init__(self, trail_history: TrialHistory, top_n=20, optimize_direction=OptimizeDirection.Minimize):
         assert trail_history is not None
         assert len(trail_history.history) > 0
         self.history = trail_history
         self.top_n = top_n
         self.samples = [t.space_sample for t in self.history.get_top(top_n)]
         self.index = 0
-        Searcher.__init__(self, None, use_meta_learner=False)
+        Searcher.__init__(self, None, use_meta_learner=False, optimize_direction=optimize_direction)
 
     @property
     def parallelizable(self):
