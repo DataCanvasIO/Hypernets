3c8ef8a7476b3c69e5c2c9f29b0efd1d4a86c489
yangjian <yangjian@zetyun.com>
2021-04-01 16:29:28 +0800

    Add grid searcher

---
+++ hypernets/searchers/grid_searcher.py
@@ -0,0 +1,67 @@
+# -*- coding:utf-8 -*-
+__author__ = 'yangjian'
+"""
+
+"""
+from ..core.searcher import Searcher, OptimizeDirection
+from ..core import EarlyStoppingError
+from sklearn.model_selection import ParameterGrid
+
+
+class GridSearcher(Searcher):
+    def __init__(self, space_fn, optimize_direction=OptimizeDirection.Minimize, space_sample_validation_fn=None,
+                 n_expansion=5):
+        Searcher.__init__(self, space_fn, optimize_direction, space_sample_validation_fn=space_sample_validation_fn)
+        space = space_fn()
+        assignable_params = space.get_unassigned_params()
+        self.grid = {}
+        self.n_expansion = n_expansion
+        for p in assignable_params:
+            self.grid[p.name] = [s.value for s in p.expansion(n_expansion)]
+        self.all_combinations = list(ParameterGrid(self.grid))
+        self.position_ = -1
+
+    @property
+    def parallelizable(self):
+        return True
+
+    def sample(self):
+        sample = self._sample_and_check(self._get_sample)
+        return sample
+
+    def _get_sample(self):
+        self.position_ += 1
+
+        if self.position_ >= len(self.all_combinations):
+            raise EarlyStoppingError('no more samples.')
+        sample = self.space_fn()
+        for k, v in self.all_combinations[self.position_].items():
+            sample.__dict__[k].assign(v)
+        assert sample.all_assigned == True
+        return sample
+
+    def get_best(self):
+        raise NotImplementedError
+
+    def update_result(self, space, result):
+        pass
+
+    def reset(self):
+        self.position_ = -1
+
+    def export(self):
+        raise NotImplementedError
+
+
+def test_parameter_grid(self):
+    space = self.get_space()
+    ps = space.get_unassigned_params()
+    grid = {}
+    for p in ps:
+        grid[p.name] = [s.value for s in p.expansion(2)]
+    all_vectors = list(ParameterGrid(grid))
+    for ps in all_vectors:
+        space = self.get_space()
+        for k, v in ps.items():
+            space.__dict__[k].assign(v)
+        assert space.all_assigned == True
---
+++ hypernets/tests/searchers/grid_test.py
@@ -0,0 +1,68 @@
+# -*- coding:utf-8 -*-
+__author__ = 'yangjian'
+"""
+
+"""
+# -*- coding:utf-8 -*-
+__author__ = 'yangjian'
+"""
+
+"""
+
+import pytest
+
+from hypernets.core.ops import *
+from hypernets.core.search_space import *
+from hypernets.searchers import GridSearcher
+from hypernets.core import EarlyStoppingError
+
+
+def get_space():
+    space = HyperSpace()
+    state = np.random.RandomState(9527)
+
+    with space.as_default():
+        id1 = Identity(p1=Choice(['a', 'b'], random_state=state),
+                       p2=Int(1, 100, random_state=state),
+                       p3=Real(0, 1.0, random_state=state))
+    return space
+
+
+class Test_GridSearcher():
+    def test_playback_searcher(self):
+        searcher = GridSearcher(get_space, n_expansion=2)
+        assert searcher.grid == {'Param_Choice_1': ['a', 'b'], 'Param_Int_1': [9, 98],
+                                 'Param_Real_1': [0.23, 0.9500000000000001]}
+
+        assert searcher.all_combinations == [{'Param_Choice_1': 'a', 'Param_Int_1': 9, 'Param_Real_1': 0.23},
+                                             {'Param_Choice_1': 'a', 'Param_Int_1': 9,
+                                              'Param_Real_1': 0.9500000000000001},
+                                             {'Param_Choice_1': 'a', 'Param_Int_1': 98, 'Param_Real_1': 0.23},
+                                             {'Param_Choice_1': 'a', 'Param_Int_1': 98,
+                                              'Param_Real_1': 0.9500000000000001},
+                                             {'Param_Choice_1': 'b', 'Param_Int_1': 9, 'Param_Real_1': 0.23},
+                                             {'Param_Choice_1': 'b', 'Param_Int_1': 9,
+                                              'Param_Real_1': 0.9500000000000001},
+                                             {'Param_Choice_1': 'b', 'Param_Int_1': 98, 'Param_Real_1': 0.23},
+                                             {'Param_Choice_1': 'b', 'Param_Int_1': 98,
+                                              'Param_Real_1': 0.9500000000000001}]
+        sample1 = searcher.sample()
+        assert sample1.vectors == [0, 9, 0.23]
+        sample2 = searcher.sample()
+        assert sample2.vectors == [0, 9, 0.9500000000000001]
+        sample3 = searcher.sample()
+        assert sample3.vectors == [0, 98, 0.23]
+        sample4 = searcher.sample()
+        assert sample4.vectors == [0, 98, 0.9500000000000001]
+        sample5 = searcher.sample()
+        assert sample5.vectors == [1, 9, 0.23]
+        sample6 = searcher.sample()
+        assert sample6.vectors == [1, 9, 0.9500000000000001]
+        sample7 = searcher.sample()
+        assert sample7.vectors == [1, 98, 0.23]
+        sample8 = searcher.sample()
+        assert sample8.vectors == [1, 98, 0.9500000000000001]
+
+        with pytest.raises(EarlyStoppingError) as ese:
+            searcher.sample()
+        assert ese.value.args[0] == 'no more samples.'
