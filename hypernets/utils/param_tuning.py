9d91e585e4bb82187d8ae1b9778c6a485385f4fd
yangjian <yangjian@zetyun.com>
2021-04-06 15:20:49 +0800

    Add kwargs to search_params

--- hypernets/utils/param_tuning.py
+++ hypernets/utils/param_tuning.py
@@ -6,7 +6,7 @@ __author__ = 'yangjian'
 
 import copy
 import time
-
+import pandas as pd
 from hypernets.conf import configure, Configurable, String, Int as cfg_int
 from hypernets.core import TrialHistory, Trial, EarlyStoppingError
 from hypernets.core.ops import Identity, HyperInput
@@ -14,6 +14,8 @@ from hypernets.core.search_space import HyperSpace, ParameterSpace
 from hypernets.searchers import EvolutionSearcher, RandomSearcher, MCTSSearcher, GridSearcher, get_searcher_cls, \
     Searcher
 from hypernets.utils import logging
+from hypernets.utils.common import isnotebook
+from IPython.display import clear_output, display, update_display
 
 logger = logging.get_logger(__name__)
 
@@ -56,16 +58,41 @@ def build_searcher(cls, func, optimize_direction='min'):
     return s
 
 
-def search_params(func, searcher='Grid', max_trials=100, optimize_direction='min'):
+def search_params(func, searcher='Grid', max_trials=100, optimize_direction='min', clear_logs=False, **func_kwargs):
     if not isinstance(searcher, Searcher):
         searcher = build_searcher(searcher, func, optimize_direction)
     retry_limit = ParamSearchCfg.trial_retry_limit
     trial_no = 1
     retry_counter = 0
     history = TrialHistory(optimize_direction)
+
+    current_trial_display_id = None
+    trials_display_id = None
     while trial_no <= max_trials:
         try:
             space_sample = searcher.sample()
+
+            if isnotebook():
+                if clear_logs:
+                    clear_output()
+                if current_trial_display_id is None:
+                    display({'text/markdown': '#### Current Trial:'}, raw=True, include=['text/markdown'])
+                    handle = display(space_sample, display_id=True)
+                    if handle is not None:
+                        current_trial_display_id = handle.display_id
+                else:
+                    update_display(space_sample, display_id=current_trial_display_id)
+                df_best_trials = pd.DataFrame([
+                    (t.trial_no, t.reward, t.elapsed, t.space_sample.vectors) for t in history.get_top(100)],
+                    columns=['Trial No.', 'Reward', 'Elapsed', 'Space Vector'])
+                if trials_display_id is None:
+                    display({'text/markdown': '#### Top trials:'}, raw=True, include=['text/markdown'])
+                    handle = display(df_best_trials, display_id=True)
+                    if handle is not None:
+                        trials_display_id = handle.display_id
+                else:
+                    update_display(df_best_trials, display_id=trials_display_id)
+
             if history.is_existed(space_sample):
                 if retry_counter >= retry_limit:
                     logger.info(f'Unable to take valid sample and exceed the retry limit {retry_limit}.')
@@ -75,9 +102,13 @@ def search_params(func, searcher='Grid', max_trials=100, optimize_direction='min
 
             ps = space_sample.get_assigned_params()
             params = {p.alias.split('.')[-1]: p.value for p in ps}
+            func_params = func_kwargs.copy()
+            func_params.update(params)
+
             trial_start = time.time()
-            last_reward = func(**params)
+            last_reward = func(**func_params)
             elapsed = time.time() - trial_start
+
             trial = Trial(space_sample, trial_no, last_reward, elapsed)
             if last_reward != 0:  # success
                 improved = history.append(trial)
