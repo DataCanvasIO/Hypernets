7522ea1c76a480e6fe6305ad4781e4f7b1b11e23
lixfz <lixf@sovon.net>
2021-06-16 13:55:39 +0800

    Override __repr__ with 'to_repr' utility

--- hypernets/core/trial.py
+++ hypernets/core/trial.py
@@ -7,10 +7,10 @@ import os
 import pickle
 import shutil
 
-from ..core.searcher import OptimizeDirection
-import math
 import pandas as pd
-from hypernets.utils.common import isnotebook
+
+from hypernets.utils.common import isnotebook, to_repr
+from ..core.searcher import OptimizeDirection
 
 
 class Trial():
@@ -24,6 +24,9 @@ class Trial():
 
         self.memo = {}
         self.iteration_scores = {}
+
+    def __repr__(self):
+        return to_repr(self)
 
     def _repr_html_(self):
         html = f'<div><h>Trial:</h>'
