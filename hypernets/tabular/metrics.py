0ab80567502e4e54a3e99ee49882b5dec441f90d
lixfz <lixf@sovon.net>
2021-07-28 23:02:28 +0800

    Add evaluation utilities

--- hypernets/tabular/metrics.py
+++ hypernets/tabular/metrics.py
@@ -2,15 +2,25 @@
 """
 
 """
+import math
+import os
+import pickle
+import sys
 
 import numpy as np
+import psutil
 from dask import dataframe as dd
+from joblib import Parallel, delayed
 from sklearn import metrics as sk_metrics
 
-from hypernets.utils import logging, const
+from hypernets.utils import const, infer_task_type, logging
 from . import dask_ex as dex
 
-logger = logging.getLogger(__name__)
+logger = logging.get_logger(__name__)
+
+_is_os_windows = sys.platform.find('win') >= 0
+
+_MIN_BATCH_SIZE = 100000
 
 _DASK_METRICS = ('accuracy', 'logloss')
 
@@ -184,3 +194,181 @@ def metric_to_scoring(metric, task=const.TASK_BINARY, pos_label=None):
         scoring = sk_metrics.get_scorer(metric2scoring[metric_lower])
 
     return scoring
+
+
+def evaluate(estimator, X, y, metrics, *, task=None, pos_label=None, classes=None,
+             average=None, threshold=0.5, n_jobs=-1):
+    assert classes is None or isinstance(classes, (list, tuple, np.ndarray))
+    if isinstance(estimator, str):
+        assert os.path.exists(estimator), f'Not found {estimator}'
+
+    if task is None and classes is not None and len(classes) >= 2:
+        task = const.TASK_BINARY if len(classes) == 2 else const.TASK_MULTICLASS
+    if task is None:
+        task, c2 = _detect_task(estimator, y)
+        if classes is None or len(classes) < 2:
+            classes = c2
+
+    n_jobs = _detect_jobs(X, n_jobs)
+    if task in {const.TASK_BINARY, const.TASK_MULTICLASS}:
+        proba = predict_proba(estimator, X, n_jobs=n_jobs)
+        pred = proba2predict(proba, task, threshold=threshold, classes=classes)
+    else:
+        pred = predict(estimator, X, n_jobs=n_jobs)
+        proba = None
+
+    if task == const.TASK_BINARY and pos_label is None:
+        pos_label = classes[-1]
+
+    if logger.is_info_enabled():
+        logger.info(f'calc_score {metrics}, task={task}, pos_label={pos_label}, classes={classes}, average={average}')
+    scores = calc_score(y_true=y, y_preds=pred, y_proba=proba, metrics=metrics,
+                        classes=classes, pos_label=pos_label, average=average)
+
+    return scores
+
+
+def predict_proba(estimator, X, *, n_jobs=-1):
+    if isinstance(estimator, str):
+        assert os.path.exists(estimator), f'Not found {estimator}'
+
+    n_jobs = _detect_jobs(X, n_jobs)
+    if logger.is_info_enabled():
+        logger.info(f'predict_proba with n_jobs={n_jobs}')
+    proba = _call_predict(estimator, 'predict_proba', X, n_jobs=n_jobs)
+
+    return proba
+
+
+def predict(estimator, X, *, task=None, classes=None, threshold=0.5, n_jobs=-1):
+    assert classes is None or isinstance(classes, (list, tuple, np.ndarray))
+    if isinstance(estimator, str):
+        assert os.path.exists(estimator), f'Not found {estimator}'
+
+    if task is None and classes is not None and len(classes) >= 2:
+        task = const.TASK_BINARY if len(classes) == 2 else const.TASK_MULTICLASS
+    if task is None:
+        task, c2 = _detect_task(estimator, None)
+        if classes is None or len(classes) < 2:
+            classes = c2
+
+    if task == const.TASK_REGRESSION:
+        if logger.is_info_enabled():
+            logger.info(f'predict with n_jobs={n_jobs}')
+        pred = _call_predict(estimator, 'predict', X, n_jobs=n_jobs)
+    else:
+        proba = predict_proba(estimator, X, n_jobs=n_jobs)
+        if task is None and (len(proba.shape) < 2 or proba.shape[1] == 1):
+            task = const.TASK_REGRESSION
+        pred = proba2predict(proba, task, threshold=threshold, classes=classes)
+    return pred
+
+
+def proba2predict(proba, task, *, threshold=0.5, classes=None):
+    assert len(proba.shape) <= 2
+
+    if logger.is_info_enabled():
+        logger.info(f'proba2predict with task={task}, classes={classes}, threshold={threshold}')
+
+    if task == const.TASK_REGRESSION:
+        return proba
+
+    if len(proba.shape) == 2:
+        if proba.shape[-1] > 2:
+            predict = proba.argmax(axis=-1)
+        else:
+            predict = (proba[:, -1] > threshold).astype(np.int32)
+    else:
+        predict = (proba > threshold).astype(np.int32)
+
+    if classes is not None:
+        if dex.is_dask_object(predict):
+            predict = dex.da.take(np.array(classes), predict, axis=0)
+        else:
+            predict = np.take(np.array(classes), predict, axis=0)
+
+    return predict
+
+
+def _detect_jobs(X, n_jobs):
+    if callable(getattr(X, 'compute', None)):  # dask data frame
+        return 1
+
+    assert X.shape[0] > 0, f'Not found data.'
+
+    if n_jobs <= 0:
+        n_jobs = math.ceil(X.shape[0] / _MIN_BATCH_SIZE)
+
+    cores = psutil.cpu_count()
+    if n_jobs > cores:
+        n_jobs = cores
+
+    return n_jobs
+
+
+def _detect_task(estimator, y):
+    if isinstance(estimator, str):
+        with open(estimator, 'rb') as f:
+            estimator = pickle.load(f)
+
+    task = None
+    classes = None
+
+    if task is None and hasattr(estimator, 'task'):
+        task = getattr(estimator, 'task', None)
+
+    if task is None and type(estimator).__name__.find('Pipeline') >= 0 and hasattr(estimator, 'steps'):
+        task = getattr(estimator.steps[-1][1], 'task', None)
+
+    if hasattr(estimator, 'classes_'):
+        classes = getattr(estimator, 'classes_', None)
+        if not (isinstance(classes, (list, tuple, np.ndarray)) and len(classes) > 1):
+            classes = None
+        if task is None and classes is not None:
+            task = const.TASK_BINARY if len(classes) == 2 else const.TASK_MULTICLASS
+
+    if classes is None and type(estimator).__name__.find('Pipeline') >= 0 and hasattr(estimator, 'steps'):
+        classes = getattr(estimator.steps[-1][1], 'classes_', None)
+        if not (isinstance(classes, (list, tuple, np.ndarray)) and len(classes) > 1):
+            classes = None
+        if task is None and classes is not None:
+            task = const.TASK_BINARY if len(classes) == 2 else const.TASK_MULTICLASS
+
+    if task is None and y is not None:
+        task, c2 = infer_task_type(y)
+        if classes is None:
+            classes = c2
+
+    return task, classes
+
+
+def _load_and_run(estimator, fn_name, df):
+    if isinstance(estimator, str):
+        logger.info(f'load estimator {estimator}')
+        with open(estimator, 'rb') as f:
+            estimator = pickle.load(f)
+
+    fn = getattr(estimator, fn_name)
+    assert callable(fn)
+
+    logger.info(f'call {fn_name}')
+    result = fn(df)
+
+    return result
+
+
+def _call_predict(estimator, fn_name, df, n_jobs=1):
+    if n_jobs > 1:
+        batch_size = math.ceil(df.shape[0] / n_jobs)
+        df_parts = [df[i:i + batch_size].copy() for i in range(df.index.start, df.index.stop, batch_size)]
+        options = dict(backend='multiprocessing') if _is_os_windows else dict(prefer='processes')
+        pss = Parallel(n_jobs=n_jobs, **options)(delayed(_load_and_run)(estimator, fn_name, x) for x in df_parts)
+
+        if len(pss[0].shape) > 1:
+            result = np.vstack(pss)
+        else:
+            result = np.hstack(pss)
+    else:
+        result = _load_and_run(estimator, fn_name, df)
+
+    return result
