4b93ad075dd1dc182baf6a234e3384e53ee6e32c
lixfz <lixf@sovon.net>
2021-12-09 19:57:01 +0800

    Import search_space and ops entities to the top level

--- hypernets/core/__init__.py
+++ hypernets/core/__init__.py
@@ -2,8 +2,11 @@
 """
 
 """
-
-from .searcher import OptimizeDirection
+from .search_space import HyperNode, HyperSpace, ParameterSpace, ModuleSpace, \
+    Int, Real, Bool, Constant, Choice, MultipleChoice, Dynamic, Cascade, get_default_space
+from .ops import HyperInput, Identity, ConnectionSpace, Optional, ModuleChoice, Sequential, Permutation, \
+    Repeat, InputChoice, ConnectLooseEnd, Reduction
+from .searcher import OptimizeDirection, Searcher
 from .callbacks import Callback, FileStorageLoggingCallback, SummaryCallback, \
     EarlyStoppingCallback, EarlyStoppingError, NotebookCallback, ProgressiveCallback
 from .trial import Trial, TrialStore, TrialHistory, DiskTrialStore
