5aafe5707cbeea9bfb497547019330d9f582fcc9
Haifeng Wu <oaksharks@outlook.com>
2022-02-14 14:10:17 +0800

    Update tests

--- hypernets/tests/utils/estimators_test.py
+++ hypernets/tests/utils/estimators_test.py
@@ -1,6 +1,9 @@
-from hypernets.utils import get_tree_importances
+import json
+
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
+
+from hypernets.utils import get_tree_importances
 
 
 def test_get_tree_importances():
@@ -11,3 +14,9 @@ def test_get_tree_importances():
     assert len(imps_dict.keys()) == 4
     for c in ['col_1', 'col_2', 'col_3', 'col_0']:
         assert c in imps_dict.keys()
+
+    values_type = list(set(map(lambda v: type(v), imps_dict.values())))
+
+    assert len(values_type) == 1
+    assert values_type[0] == int or values_type[0] == float  # not numpy type
+    assert json.dumps(imps_dict)  # has only python base type
