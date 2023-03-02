f056c100596153aa89eb0245be041aabbbd2d6ee
Haifeng Wu <oaksharks@163.com>
2023-03-02 14:16:27 +0800

    release memory usage

--- hypernets/tests/searchers/moead_searcher_test.py
+++ hypernets/tests/searchers/moead_searcher_test.py
@@ -1,3 +1,5 @@
+import time
+
 from sklearn.preprocessing import LabelEncoder
 
 from hypernets.core import OptimizeDirection
@@ -16,39 +18,59 @@ from hypernets.searchers.moead_searcher import MOEADSearcher
 import pytest
 
 
-@pytest.mark.parametrize('decomposition', ['pbi',  'weighted_sum', 'tchebicheff'])
-# @pytest.mark.parametrize('decomposition', ['tchebicheff'])
-@pytest.mark.parametrize('recombination', ["shuffle", "uniform", "single_point"])
-def test_moead_training(decomposition: str, recombination: str):
+class TestMOEADSearcher:
 
-    df = dsutils.load_bank()
-    df['y'] = LabelEncoder().fit_transform(df['y'])
+    @pytest.mark.parametrize('decomposition', ['pbi',  'weighted_sum', 'tchebicheff'])
+    @pytest.mark.parametrize('recombination', ["shuffle", "uniform", "single_point"])
+    def test_moead_training(self, decomposition: str, recombination: str):
+        t1 = time.time()
 
-    df.drop(['id'], axis=1, inplace=True)
-    X_train, X_test = train_test_split(df, test_size=0.8, random_state=1234)
+        X_train, y_train, X_test, y_test = self.data
 
-    y_train = X_train.pop('y')
-    y_test = X_test.pop('y')
+        search_space = PlainSearchSpace(enable_dt=True, enable_lr=False, enable_nn=True)
 
-    search_space = PlainSearchSpace(enable_dt=True, enable_lr=False, enable_nn=True)
+        objectives = [ElapsedObjective(),
+                      PredictionObjective('logloss', OptimizeDirection.Minimize)]
 
-    objectives = [ElapsedObjective(),
-                  PredictionObjective('logloss', OptimizeDirection.Minimize)]
+        rs = MOEADSearcher(search_space, objectives=objectives,
+                           decomposition=decomposition, recombination=recombination, n_sampling=2)
 
-    rs = MOEADSearcher(search_space, objectives=objectives,
-                       decomposition=decomposition, recombination=recombination, n_sampling=2)
+        hk = PlainModel(rs, task='binary', callbacks=[SummaryCallback()], transformer=MultiLabelEncoder)
+        # N => C_3^1
+        assert rs.population_size == 3
+        hk.search(X_train, y_train, X_test, y_test, max_trials=8)
 
-    hk = PlainModel(rs, task='binary', callbacks=[SummaryCallback()], transformer=MultiLabelEncoder)
+        len(hk.history.trials)
+        assert hk.get_best_trial()
+        rs.plot_pf()
+        print(time.time() - t1)
 
-    hk.search(X_train, y_train, X_test, y_test, max_trials=10)
+    @classmethod
+    def setup_class(cls):
+        df = dsutils.load_bank().sample(1000)
+        df['y'] = LabelEncoder().fit_transform(df['y'])
 
-    len(hk.history.trials)
-    assert hk.get_best_trial()
-    rs.plot_pf()
+        df.drop(['id'], axis=1, inplace=True)
+        X_train, X_test = train_test_split(df, test_size=0.8, random_state=1234)
+
+        y_train = X_train.pop('y')
+        y_test = X_test.pop('y')
+
+        cls.data = (X_train, y_train, X_test, y_test)
+
+    @classmethod
+    def teardown_class(cls):
+        del cls.data
 
 
 
 if __name__ == '__main__':
     # test_moead_training("tchebicheff", "shuffle")
     # test_moead_training("tchebicheff", "single_point")
-    test_moead_training("tchebicheff", "uniform")
+    # test_moead_training("tchebicheff", "shuffle")
+    tm = TestMOEADSearcher()
+    tm.setup_class()
+    tm.test_moead_training("weighted_sum", "shuffle")
+    tm.teardown_class()
+
+
