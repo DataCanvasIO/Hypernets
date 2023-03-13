684ea8eb105a451321a4137e4144aada4077d0e5
Haifeng Wu <oaksharks@163.com>
2023-03-13 17:39:09 +0800

    using naive logloss

--- hypernets/model/objectives.py
+++ hypernets/model/objectives.py
@@ -1,5 +1,6 @@
 import abc
 
+import numpy as np
 from hypernets.core.objective import Objective
 from hypernets.utils import const
 
@@ -23,22 +24,31 @@ class ElapsedObjective(PerformanceObjective):
 
 class PredictionObjective(PerformanceObjective):
 
-    def __init__(self, name, scorer):
-        direction = 'max' if scorer._sign > 0 else 'min'
+    def __init__(self, name, scorer, direction=None):
+        if direction is None:
+            direction = 'max' if scorer._sign > 0 else 'min'
+
         super(PredictionObjective, self).__init__(name, direction=direction)
         self._scorer = scorer
 
     def call(self, trial, estimator, X_test, y_test, **kwargs):
-        import numpy as np
         #y_pred = estimator.predict(X_test)
         #y_proba = estimator.predict_proba(X_test)
-        return np.absolute(self._scorer(estimator, X_test, y_test))
+        value = self._scorer(estimator, X_test, y_test)
+        return value
 
     @staticmethod
     def create(name, task=const.TASK_BINARY, pos_label=None):
-        from hypernets.tabular.metrics import metric_to_scoring
-        scorer = metric_to_scoring(metric=name, task=task, pos_label=pos_label)
-        return PredictionObjective(name, scorer)
+        if name.lower() == 'logloss':
+            # Note: the logloss score in sklearn is negative of naive logloss to maximize optimization
+            from sklearn.metrics import log_loss, make_scorer
+            scorer = make_scorer(log_loss, greater_is_better=True, needs_proba=True)  # let _sign>0
+            return PredictionObjective(name, scorer, direction='min')
+        else:
+            from hypernets.tabular.metrics import metric_to_scoring
+            scorer = metric_to_scoring(metric=name, task=task, pos_label=pos_label)
+            return PredictionObjective(name, scorer, direction='min')
+
 
 
 class FeatureComplexityObjective(ComplexityObjective):
