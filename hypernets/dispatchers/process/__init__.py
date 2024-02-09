da0220307dd9c7fb2993f49d38775d73c0dd9fc2
lixfz <lixf@sovon.net>
2024-02-10 00:01:53 +0800

    Add warning msg

--- hypernets/dispatchers/process/__init__.py
+++ hypernets/dispatchers/process/__init__.py
@@ -6,6 +6,12 @@ try:
     from .grpc_process import GrpcProcess
 except ImportError:
     pass
+except:
+    from hypernets.utils import logging
+    import sys
+
+    logger = logging.get_logger(__name__)
+    logger.warning('Failed to load GrpcProcess', exc_info=sys.exc_info())
 
 try:
     from .ssh_process import SshProcess
